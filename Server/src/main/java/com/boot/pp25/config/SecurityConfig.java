60da6435bb5178902c24b09b33a247c9f90eb108
Epanechnukov ILya <59977339+ScriptingGuy1@users.noreply.github.com>
2020-05-18 11:43:05 +0300

    fix bugs wth SS

--- Server/src/main/java/com/boot/pp25/config/SecurityConfig.java
+++ Server/src/main/java/com/boot/pp25/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.boot.pp25.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ import java.util.logging.Logger;
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan("com.boot.pp25")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static Logger log = Logger.getLogger(SecurityConfig.class.getName());
@@ -22,23 +24,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
         log.info("configureGlobalSecurity");
-        auth.inMemoryAuthentication().withUser("serverSecretAdmin")
+        auth.inMemoryAuthentication()
+                .withUser("serverSecretAdmin")
                 .password(passwordEncoder.encode("secretPassword"))
                 .roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("serverSecretUser")
+        auth.inMemoryAuthentication()
+                .withUser("serverSecretUser")
                 .password(passwordEncoder.encode("secretPassword"))
                 .roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api**").hasRole("ADMIN")
+        log.info("configure(HttpSecurity http)");
+        http.
+                httpBasic()
                 .and()
-                .httpBasic();
+                .authorizeRequests()
+                .antMatchers("/api/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
     }
 
 
